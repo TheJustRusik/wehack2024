5f21f954e4ba7559403e350820bc40a277aa2382
TheJustRusik <7thejustrusik7@gmail.com>
2024-05-04 13:02:39 +0500

    First commit🥇

---
+++ src/test/java/org/kenuki/wehack2024/Wehack2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kenuki.wehack2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wehack2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
